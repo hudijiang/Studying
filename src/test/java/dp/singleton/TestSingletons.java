1d03ac16bc5f86196e9fc152f9eb9723065fd988
hudijiang <343010985@qq.com>
2020-06-12 16:48:24 +0800

    单元测试

--- src/test/java/dp/singleton/TestSingletons.java
+++ src/test/java/dp/singleton/TestSingletons.java
@@ -2,16 +2,23 @@ package dp.singleton;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 public class TestSingletons {
     private static HashMap DATA = new HashMap();
     private static Integer SIZE = 100;
+
+    //饿汉模式单元测试
     @Test
-    public void testGetInstanceHungry() throws  Exception{
+    public void testGetSingletonHungry() throws  Exception{
 
         for (int i=0;i<=SIZE;i++) {
-            SingletonLazyHungry singletonLazyHungry = SingletonLazyHungry.getInstance();
+            SingletonHungry singletonLazyHungry = SingletonHungry.getInstance();
             new Thread(
                     () -> {
                         Assert.assertEquals(singletonLazyHungry,SingletonHungry.getInstance());
@@ -19,6 +26,26 @@ public class TestSingletons {
             ).start();
 
         }
+    }
+
+    //饿汉单例反序列化测试
+    @Test
+    public void testGetSingletonHungry2()throws Exception{
+        SingletonHungry singletonEnum = SingletonHungry.getInstance();
+        FileOutputStream fos = new FileOutputStream("Enum Object");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(singletonEnum);
+        oos.flush();
+        fos.close();
+
+
+        SingletonHungry singletonEnum1 = null;
+        FileInputStream fis = new FileInputStream("Enum Object");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        singletonEnum1 = (SingletonHungry)ois.readObject();
+        ois.close();
+        //反序列化后2个对象不相等
+        Assert.assertNotEquals(singletonEnum,singletonEnum1);
     }
 
     @Test
@@ -34,6 +61,26 @@ public class TestSingletons {
         }
     }
 
+    //懒汉单例反序列化测试
+    @Test
+    public void testGetInstanceLazyHungry2()throws Exception{
+        SingletonLazyHungry singletonEnum = SingletonLazyHungry.getInstance();
+        FileOutputStream fos = new FileOutputStream("Enum Object");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(singletonEnum);
+        oos.flush();
+        fos.close();
+
+
+        SingletonLazyHungry singletonEnum1 = null;
+        FileInputStream fis = new FileInputStream("Enum Object");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        singletonEnum1 = (SingletonLazyHungry)ois.readObject();
+        ois.close();
+        //反序列化后2个对象不相等
+        Assert.assertNotEquals(singletonEnum,singletonEnum1);
+    }
+
     @Test
     public void testGetInstanceEnum() throws  Exception{
         SingletonEnum singletonEnum = SingletonEnum.getInstances();
@@ -46,4 +93,26 @@ public class TestSingletons {
 
         }
     }
+
+    //单例反序列化测试
+    @Test
+    public void testGetInstanceEnum2()throws Exception{
+        SingletonEnum singletonEnum = SingletonEnum.getInstances();
+        singletonEnum.setData(new Object());
+        FileOutputStream fos = new FileOutputStream("Enum Object");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(singletonEnum);
+        oos.flush();
+        fos.close();
+
+
+        SingletonEnum singletonEnum1 = null;
+        FileInputStream fis = new FileInputStream("Enum Object");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        singletonEnum1 = (SingletonEnum)ois.readObject();
+        ois.close();
+
+        Assert.assertEquals(singletonEnum,singletonEnum1);
+    }
 }
+
